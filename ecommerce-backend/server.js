4be02b424f7270c2ce8f12f26af27b65026685ec
Alex <Alex@gmail.com>
2025-05-30 19:23:14 +0530

    mongo db connection issue fix

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const serverless = require('serverless-http');
 require('dotenv').config();
 
 const orderRoutes = require('./routes/orderRoutes');
@@ -9,19 +8,14 @@ const orderRoutes = require('./routes/orderRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/orders', orderRoutes);
 
-let isConnected = false;
-
-async function connectToDatabase() {
-  if (isConnected) return;
-  await mongoose.connect(process.env.MONGO_URI);
-  isConnected = true;
-}
-
-const handler = serverless(async (req, res) => {
-  await connectToDatabase();
-  return app(req, res);
-});
-
-module.exports = handler;
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT || 5000, () => {
+      console.log('Server running');
+    });
+  })
+  .catch(err => console.error(err));

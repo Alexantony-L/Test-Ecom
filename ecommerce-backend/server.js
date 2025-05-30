d037e18195874e4d1171c1549257bdd982999025
Alex <Alex@gmail.com>
2025-05-30 19:20:21 +0530

    vercel deployment issue fix

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -5,8 +5,8 @@ const serverless = require('serverless-http');
 require('dotenv').config();
 
 const orderRoutes = require('./routes/orderRoutes');
-const app = express();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/orders', orderRoutes);
@@ -15,20 +15,13 @@ let isConnected = false;
 
 async function connectToDatabase() {
   if (isConnected) return;
-  console.log('Connecting to MongoDB...');
   await mongoose.connect(process.env.MONGO_URI);
   isConnected = true;
-  console.log('MongoDB connected');
 }
 
 const handler = serverless(async (req, res) => {
-  try {
-    await connectToDatabase();
-    return app(req, res);
-  } catch (err) {
-    console.error('Error in handler:', err);
-    res.status(500).send('Internal Server Error');
-  }
+  await connectToDatabase();
+  return app(req, res);
 });
 
 module.exports = handler;
